b9b56a8950137991a81ca589cf7d9e461bc8907d
yuhui-zh15 <yuhui-zh15@mails.tsinghua.edu.cn>
2018-03-26 00:16:02 +0800

    lab1/ex1

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -12,7 +12,7 @@
  *  Please refer to Page 196~198, Section 8.2 of Yan Wei Min's Chinese book
  * "Data Structure -- C programming language".
 */
-// LAB2 EXERCISE 1: YOUR CODE
+// LAB2 EXERCISE 1: 2015011372
 // you should rewrite functions: `default_init`, `default_init_memmap`,
 // `default_alloc_pages`, `default_free_pages`.
 /*
@@ -159,8 +159,10 @@ default_free_pages(struct Page *base, size_t n) {
     base->property = n;
     SetPageProperty(base);
     list_entry_t *le = list_next(&free_list);
+    list_entry_t *last = &free_list;
     while (le != &free_list) {
         p = le2page(le, page_link);
+        if (p < base && !(p + p->property == base)) last = le;
         le = list_next(le);
         if (base + base->property == p) {
             base->property += p->property;
@@ -175,7 +177,7 @@ default_free_pages(struct Page *base, size_t n) {
         }
     }
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    list_add(last, &(base->page_link));
 }
 
 static size_t
