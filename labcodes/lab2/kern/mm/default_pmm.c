2fa3a0219cb839435977dc08a3576fc5c0a6a73a
yuhui-zh15 <yuhui-zh15@mails.tsinghua.edu.cn>
2018-03-26 11:13:23 +0800

    lab2/ex1

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -135,12 +135,14 @@ default_alloc_pages(size_t n) {
         }
     }
     if (page != NULL) {
-        list_del(&(page->page_link));
+        
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+            list_add(&(page->page_link), &(p->page_link));
+        }
+        
+        list_del(&(page->page_link));
         nr_free -= n;
         ClearPageProperty(page);
     }
